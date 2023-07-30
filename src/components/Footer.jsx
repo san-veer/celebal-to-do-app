8418f733ce9e866e312734882ae22bea7769d8b1
Sanvir <75733566+Sanvir13@users.noreply.github.com>
2023-07-30 23:48:07 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>Soumyadeep Saha || CSI ID : CT-CSI23/RJ0500</p>
+    </footer>
+  );
+}
+
+export default Footer;
